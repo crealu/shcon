d44eeb56a83966405393e934da893cb7aa49bbd2
crealu <cameron.miller294@gmail.com>
2024-12-24 16:27:19 -0800

    Create shader 08
    - Create shader 08 that contains rotating circles

---
+++ public/shaders/08/fs.glsl
@@ -0,0 +1,29 @@
+precision mediump float;
+
+uniform vec3 u_resolution;
+uniform float u_time;
+uniform float u_moon;
+
+void main() {
+  vec2 view = 2.0 * gl_FragCoord.xy - u_resolution.xy;
+  float axis = u_resolution.y;
+  vec2 field = view / axis;
+
+  float radius = 0.2;
+  vec3 color = vec3(0.99, 0.87, 0.20);
+  vec3 color1 = vec3(0.86, 0.63, 0.82);
+
+  for (int i = 0; i < 10; i++) {
+    float circle = length(field) - radius;
+    float x = sin((float(i) / 5.0) * u_time);
+    float y = cos((float(i) / 5.0) * u_time);    
+    vec2 offset = vec2(x, y);
+
+    field += offset / 2.0;
+    color /= abs(1.0 - circle);
+    circle = fract(circle) * 1.0;
+    color = mix(color, color1, circle);
+  }
+
+    gl_FragColor = vec4(color, 1.0);
+}
