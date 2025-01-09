d145bd6ae1588200be6c3adcde3152c8e642b89d
crealu <cameron.miller294@gmail.com>
2025-01-09 09:05:28 -0800

    Update ShaderSetup.js
    - Revise ShaderSetup definition to automate the loading of shader programs that use time and a user specified resolution

--- public/js/ShaderSetup.js
+++ public/js/ShaderSetup.js
@@ -1,15 +1,30 @@
 class ShaderSetup {
-	constructor(canvasElement, vertexShader, fragmentShader) {
-		this.canvas = canvasElement;
-		this.gl = null;
+	constructor(gl, canvas) {
 		this.program = null;
+		this.gl = gl;
+		this.vs = null;
+		this.fs = null;
+		this.buffers = null;
+		this.positions = [];
+
+		this.canvas = canvas;
+		this.uTime = '';
 		this.time = 0.0;
 		this.limit = 12;
-		this.uTime = '';
 		this.id = '';
-		this.vs = vertexShader;
-		this.fs = fragmentShader;
-		this.setup();
+	}
+
+	initProgram(vsSource, fsSource) {
+		this.vs = loadShader(this.gl, this.gl.VERTEX_SHADER, vsSource);
+		this.fs = loadShader(this.gl, this.gl.FRAGMENT_SHADER, fsSource);
+
+		const shaderProgram = this.gl.createProgram();
+		this.gl.attachShader(shaderProgram, vertexShader);
+		this.gl.attachShader(shaderProgram, fragmentShader);
+		this.gl.linkProgram(shaderProgram);
+		this.gl.useProgram(shaderProgram);
+
+		this.program = shaderProgram;
 	}
 
 	loadShader(gl, type, source) {
@@ -25,73 +40,72 @@ class ShaderSetup {
 		return shader;
 	}
 
-	initProgram(vsSource, fsSource) {
-		const vertexShader = loadShader(this.gl, this.gl.VERTEX_SHADER, vsSource);
-		const fragmentShader = loadShader(this.gl, this.gl.FRAGMENT_SHADER, fsSource);
-
-		const shaderProgram = gl.createProgram();
-		this.gl.attachShader(shaderProgram, vertexShader);
-		this.gl.attachShader(shaderProgram, fragmentShader);
-		this.gl.linkProgram(shaderProgram);
-		this.gl.useProgram(shaderProgram);
-
-		return shaderProgram;
-	}
-
 	initBuffers() {
+		this.positions = [-1, -1, -1, 3, 3, -1];
+
 		const positionBuffer = gl.createBuffer();
 		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, positionBuffer);
-		this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array([-1, -1, -1, 3, 3, -1]), this.gl.STATIC_DRAW);
+		this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(this.positions), this.gl.STATIC_DRAW);
+	
+		this.buffers = {
+			position: positionBuffer
+		}
 	}
 
-	initLocation() {
+	initLocations() {
+		this.gl.useProgram(this.program);
+
 		const positionLocation = gl.getAttribLocation(this.program, "a_position");
-		this.gl.enableVertexAttribArray(positionLocation);
 		this.gl.vertexAttribPointer(positionLocation, 2, this.gl.FLOAT, false, 0, 0);  
-	}
+		this.gl.enableVertexAttribArray(positionLocation);
 
-	initUniforms() {
 		const uResolution = this.gl.getUniformLocation(this.program, 'u_resolution');
-		this.gl.uniform3f(uResolution, canvas.width, canvas.height, 1.0);
+		this.gl.uniform3f(uResolution, this.canvas.width, this.canvas.height, 1.0);
 
 		const uniformTime =this. gl.getUniformLocation(this.program, 'u_time');
 		this.gl.uniform1f(uniformTime, time);
 
-		return uniformTime;
+		this.uTime = uniformTime;	
 	}
 
 	setup() {
-		this.gl = this.canvas.getContext('webgl');
-		this.program = initProgram(this.vs, this.fs);
-		this.initBuffer();
-		this.initLocation();
-		this.uTime = this.initUniforms();
+		this.initProgram(this.vs, this.fs);
+		this.initBuffers();
+		this.initLocations();
+		this.render();
+	}
 
+	reset(vs, fs) {
+		this.initProgram(vs, fs);
+		this.initBuffers();
+		this.initLocations();
+		this.render();
+	}
+
+	clear() {
 		this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
 		this.gl.clearDepth(1.0);
 		this.gl.enable(this.gl.DEPTH_TEST);
 		this.gl.depthFunc(this.gl.LEQUAL);
 	}
 
-	clear() {
-
+	pause() {
+		window.cancelAnimationFrame(this.id);
 	}
 
 	render() {
+		this.clear();
+
 		this.time += 0.01;
 
-		this.gl.uniform1f(this.uniformTime, this.time);
+		this.gl.uniform1f(this.uTime, this.time);
 		this.gl.drawArrays(this.gl.TRIANGLES, 0, 3);  
 
 		if (this.time >= this.limit) {
-			window.cancelAnimationFrame(this.render);
+			this.pause();
 			return;
 		}
 
-		this.id = window.requestAnimationFrame(this.render);
-	}
-
-	pause() {
-		window.cancelAnimationFrame(this.id);
+		this.id = window.requestAnimationFrame(() => this.render());
 	}
 }
