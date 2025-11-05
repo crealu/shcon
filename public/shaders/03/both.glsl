d4a12ca38aa87dbedd5956099834de3ef186eeb4
crealu <cameron.miller294@gmail.com>
2025-11-04 19:57:46 -0800

    Update both.glsl
    - Remove the shadow animation of the logo

--- public/shaders/03/both.glsl
+++ public/shaders/03/both.glsl
@@ -68,43 +68,43 @@ vec3 mf(vec2 field, vec3 color) {
   field += vec2(0.5, 0.3);
   float c = circle2(field, 0.2, color);
   color *= smoothstep(0.1, s, c);
-  color = shadow(color, c);
+  // color = shadow(color, c);
   field -= vec2(0.7, 0.0);
 
   field += vec2(0.55, -0.4);
   float b1 = box(field, vec2(0.05, 0.4));
   color *= smoothstep(0.1, s, b1);
-  color = shadow(color, b1);
+  // color = shadow(color, b1);
   field -= vec2(0.55, -0.4);
 
   field += vec2(-0.2, -0.4);
   float b2 = box(field, vec2(0.05, 0.4));
   color *= smoothstep(0.1, s, b2);
-  color = shadow(color, b2);
+  // color = shadow(color, b2);
   field -= vec2(-0.2, 0.4);
 
   field += vec2(0.0, 0.2);
   float p1 = parallelogram(field, 0.05, 0.2, 0.15);
   color *= smoothstep(0.1, s, p1);
-  color = shadow(color, p1);
+  // color = shadow(color, p1);
   field -= vec2(0.0, 0.2);
 
   field += vec2(0.35, 0.2);
   float p2 = parallelogram(field, 0.05, 0.2, -0.15);
   color *= smoothstep(0.1, s, p2);
-  color = shadow(color, p2);
+  // color = shadow(color, p2);
   field -= vec2(0.35, 0.2);
   
   field += vec2(-0.4, 0.4);
   float b3 = box(field, vec2(0.15, 0.025));
   color *= smoothstep(0.1, s, b3);
-  color = shadow(color, b3);
+  // color = shadow(color, b3);
   field -= vec2(-0.4, 0.4);
 
   field += vec2(-0.4, 0.025);
   float b4 = box(field, vec2(0.15, 0.025));
   color *= smoothstep(0.1, s, b4);
-  color = shadow(color, b4);
+  // color = shadow(color, b4);
 
   return color;
 }
@@ -117,13 +117,13 @@ void main() {
   vec3 color = vec3(0.5, 0.0, 0.0);
   color = mf(field, color);
 
-  for (float i = 1.0; i < 2.0; i++) {
-    // field = fract(field * 1.5) - 0.5;
-    // float co = sin(fract(field.x * field.y) * 4.0 + u_time) / 8.0;
-    float d = dot(cos(u_time), field.x*field.y);
-    // color += smoothstep(0.1, 0.2, co);
-    color = mix(color, vec3(0.0, 0.0, 0.5), d);
-  }
+  // for (float i = 1.0; i < 2.0; i++) {
+  //   // field = fract(field * 1.5) - 0.5;
+  //   // float co = sin(fract(field.x * field.y) * 4.0 + u_time) / 8.0;
+  //   float d = dot(cos(u_time), field.x * field.y);
+  //   // color += smoothstep(0.1, 0.2, co);
+  //   // color = mix(color, vec3(0.0, 0.0, 0.5), d);
+  // }
   
   gl_FragColor = vec4(color, 1.0);
 }
